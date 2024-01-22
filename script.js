a66322d1051cbb2e794bd843e8f33879e50249c4
hazalsigic <hazalsigic@hotmail.com>
2024-01-22 16:37:47 +0000

    localStorage setItem code added

--- script.js
+++ script.js
@@ -18,24 +18,23 @@ function colorCode() {
     var currentTime = dayjs().format("H");
 
     $('tr').each(function(i, el) {
-        var $tableRow = $(el);
-        var $tableHeading = $tableRow.find('th');
-        var $tableEvent = $tableRow.find('.event');
-        var thisId = $tableHeading.attr('id');
+        var tableRow = $(el);
+        var tableEvent = tableRow.find('.event');
+        var thisId = tableRow.attr('id');
         var thisHourString = thisId.replace('h-', '');
         var thisRowHour = parseInt(thisHourString);
         if (thisRowHour == currentTime) {
-            $tableEvent.addClass('present');
-            $tableEvent.removeClass('future');
-            $tableEvent.removeClass('past');
+            tableEvent.addClass('present');
+            tableEvent.removeClass('future');
+            tableEvent.removeClass('past');
         } else if (thisRowHour > currentTime) {
-            $tableEvent.addClass('future');
-            $tableEvent.removeClass('past');
-            $tableEvent.removeClass('present');
+            tableEvent.addClass('future');
+            tableEvent.removeClass('past');
+            tableEvent.removeClass('present');
         } else {
-            $tableEvent.addClass('past');
-            $tableEvent.removeClass('present');
-            $tableEvent.removeClass('future');
+            tableEvent.addClass('past');
+            tableEvent.removeClass('present');
+            tableEvent.removeClass('future');
         }
     });
     
@@ -46,9 +45,36 @@ setInterval(colorCode, 1000);
 // event adding function
 
 var timeBlock = $('.row');
-var eventBlock = $('.event')
+var eventBlock = $('.event');
+
 timeBlock.on('click', function(){
     if (!eventBlock.find('textarea').length) {
         eventBlock.append('<textarea></textarea>');
+        var textName = $(this).attr('id');
+        $('textarea').attr('class', textName);
+        $(this).children(".event").addClass(textName);
+
     }
 });
+
+//Saving to local storage
+
+$('.saveBtn').on('click', function () {
+
+    var text = $(this).siblings('.event').children('textarea').val();
+    var time = $(this).parent().attr('id');
+    console.log(text);
+    console.log(time);
+ 
+    localStorage.setItem(time, text);
+})
+
+
+// Getting event from local storage in case of a page refresh
+
+var h10 = localStorage.getItem("h-10")
+
+console.log(h10)
+
+//$("h-10").children(".event").text(h10);
+
